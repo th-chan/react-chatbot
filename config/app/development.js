121d63f88e61d51d14468786b491fe81114029e9
TH Chan <th.chan@accenture.com>
2021-02-05 09:50:19 +0800

    update project settings

---
+++ config/app/development.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // 'API_HOST': 'https://52.220.23.76',
+    // 'API_VERSION': '/api/v1/',
+    'WEB_ROOT': '/',
+    'NOT_FOUND_PAGE':'/404',
+    'REACT_HOMEPAGE': 'home.html'
+};
